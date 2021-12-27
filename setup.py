404954bb083a8e8cadc40eed01a75e1b276cd938
rozhkovdmitrii <rozhkovdmitrii@infatica.io>
2021-12-27 14:04:50 +0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='logged-based-class',
+      version='1.0',
+      description='Per class logging tools',
+      author='Rozhkov Dmitrii',
+      author_email='rozhkovdmitrii@yandex.ru',
+      license='GPL-3.0',
+      url='http://rozhkovdmitrii.ru/',
+      packages=["logged_based_class"],
+      install_requires=['singleton-decorator'])
